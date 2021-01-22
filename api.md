4e93226dda0cee2f7b3475f68b2f4d181b3ddb7f
aekostyuk <a.e.kostyuk@gmail.com>
2021-01-22 07:01:03 +0300

    Base functions

---
+++ api.md
@@ -0,0 +1,15 @@
+### API key
+
+"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjk1YWExODBhYzBmYzA2Njk0YjI5MjhiZTQ5MjEwZiIsInN1YiI6IjYwMGExMGY2ZDU1YzNkMDAzZTZjZTFkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.g0_PFLJolVDl0TxKvgrbljkDRn_coTxJbPkUAOd6t1g"
+
+### API фильмов
+
+"https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
+
+### База картинок
+
+"https://image.tmdb.org/t/p/w1280"
+
+### API поиска
+
+"https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Movie from './components/Movie';
+
 import './App.css';
 
+const APIKEY = "3295aa180ac0fc06694b2928be49210f";
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${APIKEY}&language=ru&page=1`;
+
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${APIKEY}&query=`;
+
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+
+  const getMovies = (API) => {
+    fetch(API)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setMovies(data.results);
+    });
+  }
+  
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+
+      setSearchTerm('');
+    }
+  }
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <header>
+      <form onSubmit={handleOnSubmit}>
+        <input  className="search" type="search"
+        placeholder="Поиск..."
+        value={searchTerm}
+        onChange={handleOnChange}/>
+      </form>
+    </header>
+    <main id="main">
+      {movies.length > 0 && movies.map(movie => (
+          <Movie key={movie.id} {...movie}/>
+        ))}
+    </main>
+  </>
   );
 }
 
